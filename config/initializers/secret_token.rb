212b6a9cbfb23c0403013452b262f5746d06ecac
Ben Klein <bk.romulox@gmail.com>
2013-07-17 23:36:59 -0400

    freshly made rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Copypasta::Application.config.secret_token = '6f3bd18b789cc1905b1ab029c10dc5ba248caebfdffb8f9eb30a29753d1b6709792f110d3e5f46991136c4899ab3bf955012350c06874c449fbf868e705ebd17'
